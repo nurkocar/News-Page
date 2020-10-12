55b7b67bf919c428358b5e24aa3c7d60c7307639
Nur Kocar <64444237+nurkocar@users.noreply.github.com>
2020-10-12 18:50:25 +0000

    start news project

---
+++ src/News.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const News = () => {
+    return(
+        <View>
+            <View>
+                <Text>News</Text>
+            </View>
+        </View>
+    )
+};
+
+export default News;
